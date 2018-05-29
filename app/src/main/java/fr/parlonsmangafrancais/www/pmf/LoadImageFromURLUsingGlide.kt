f96cdb6644478cac40977d5453d15f761e50c20d
Marie <Mariepolmontiel@gmail.com>
2018-05-29 15:20:49 +0900

    try fix Inflate problem

--- app/src/main/java/fr/parlonsmangafrancais/www/pmf/LoadImageFromURLUsingGlide.kt
+++ app/src/main/java/fr/parlonsmangafrancais/www/pmf/LoadImageFromURLUsingGlide.kt
@@ -3,6 +3,7 @@ package fr.parlonsmangafrancais.www.pmf
 import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.LinearLayout
 import com.bumptech.glide.Glide
@@ -13,9 +14,31 @@ class LoadImageFromURLUsingGlide : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val linearLayout = findViewById<LinearLayout>(R.id.imageManga)
+        val linearLayout = findViewById<LinearLayout>(R.id.linearLayout)
+
         val imageView = ImageView(this)
-        Glide.with(this).load("https://s3.amazonaws.com/appsdeveloperblog/Micky.jpg").into(imageView)
+        Glide.with(this)
+
+
+                .error(R.drawable.imagenotfound)
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        // log exception
+                        Log.e("TAG", "Error loading image", e);
+                        return false; // important to return false so the error placeholder can be placed
+                    }
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        return false;
+                    }
+                })
+
+
+
+
+                .load("https://s3.amazonaws.com/appsdeveloperblog/Micky.jpg")
+                .into(imageView)
         linearLayout.addView(imageView)
     }
 }

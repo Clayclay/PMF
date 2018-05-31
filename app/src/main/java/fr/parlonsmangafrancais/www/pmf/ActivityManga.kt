62681a912fea5bf5163c566c8bf34c20d6f85522
Marie <Mariepolmontiel@gmail.com>
2018-05-31 21:42:50 +0900

    Probleme injection de la classe manga dans tableau

--- app/src/main/java/fr/parlonsmangafrancais/www/pmf/ActivityManga.kt
+++ app/src/main/java/fr/parlonsmangafrancais/www/pmf/ActivityManga.kt
@@ -8,6 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 
 class ActivityManga : AppCompatActivity() {
@@ -19,18 +22,35 @@ class ActivityManga : AppCompatActivity() {
         setContentView(R.layout.list_manga)
 
         val lv = findViewById(R.id.manga_list_view) as ListView
-        lv.adapter = ListExampleAdapter(this)
-    }
+        lv.adapter = ListExampleAdapter(this)   }
 
     private class ListExampleAdapter(context: Context) : BaseAdapter() {
 
 
 
-        internal var sList = arrayOf("CupCake", "Donut", "Eclair", "Froyo", "GingerBread", "HoneyComb", "IceCream Sandwich",
-                "Jelly Bean", "KitKat", "Lollipop", "Marshmallow", "Nougat")
+        fun sList   (args: Manga) {
+          var mangas: ArrayList<Manga> = ArrayList()
+         mangas.add(Manga(1,"trouducul"))
+         mangas.add(Manga(2,"chapeaupointu"))
+         }
+
+        // internal var sList = arrayOf("CupCake", "Donut", "Eclair", "Froyo", "GingerBread", "HoneyComb", "IceCream Sandwich",
+        //  "Jelly Bean", "KitKat", "Lollipop", "Marshmallow", "Nougat")
+
+
+        internal var sList = arrayOf( mangas.name        )
+        
+        //data class Manga (val name: String = "", val id: Int = 0)
+
+       // fun sList(args: Array<String>) {        Manga1   ,  Manga2 }
+
+
+      //var Manga1 = Manga (1,"trouducul")
+       // var Manga2 = Manga (2,"chapeaupointu")
 
 
 
+        //internal var sList = arrayOf(Manga1,Manga2)
 
 
         private val mInflator: LayoutInflater
@@ -45,6 +65,7 @@ class ActivityManga : AppCompatActivity() {
 
         override fun getItem(position: Int): Any {
             return sList[position]
+
         }
 
         override fun getItemId(position: Int): Long {
@@ -63,7 +84,12 @@ class ActivityManga : AppCompatActivity() {
                 vh = view.tag as ListRowHolder
             }
 
+
             vh.label.text = sList[position]
+
+
+           // this.Manga1 = view?.findViewById<TextView>(R.id.manganame) as Manga
+
             return view
         }
     }
--- app/src/main/java/fr/parlonsmangafrancais/www/pmf/Manga.kt
+++ app/src/main/java/fr/parlonsmangafrancais/www/pmf/Manga.kt
@@ -3,12 +3,6 @@ package fr.parlonsmangafrancais.www.pmf
 import android.media.Image
 import java.util.*
 
-data class Manga constructor (val name : String ) {
-
-    //var resume : String = ""
-    //val imageUrl: String = ""
-   // var note : Int = 0
-    //@Json(name = "like_count") val likeCount: Int = 0
-   // @Json(name = "date_futur") val dateFutur: Date = Date(0)
+data class Manga constructor (val id: Long,var name : String ) {
 
 }

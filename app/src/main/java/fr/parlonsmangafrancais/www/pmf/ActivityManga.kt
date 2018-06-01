8ab226cc827ec8b484df252568c13abe94dcb4f5
Marie <Mariepolmontiel@gmail.com>
2018-06-01 12:19:59 +0900

    Probleme de tableau

--- app/src/main/java/fr/parlonsmangafrancais/www/pmf/ActivityManga.kt
+++ app/src/main/java/fr/parlonsmangafrancais/www/pmf/ActivityManga.kt
@@ -8,13 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
-
+import fr.parlonsmangafrancais.www.pmf.R.layout.manga
 
 
 class ActivityManga : AppCompatActivity() {
-
 
 
     override fun onCreate(savedState: Bundle?) {
@@ -22,86 +19,76 @@ class ActivityManga : AppCompatActivity() {
         setContentView(R.layout.list_manga)
 
         val lv = findViewById(R.id.manga_list_view) as ListView
-        lv.adapter = ListExampleAdapter(this)   }
+        lv.adapter = ListExampleAdapter(this)
+    }
 
     private class ListExampleAdapter(context: Context) : BaseAdapter() {
 
 
+       // fun getMangaInfo() = {
 
-        fun sList   (args: Manga) {
-          var mangas: ArrayList<Manga> = ArrayList()
-         mangas.add(Manga(1,"trouducul"))
-         mangas.add(Manga(2,"chapeaupointu"))
-         }
+         //   val manga = Manga("Epic Games", 1)
+           // val (name, id) = manga
 
-        // internal var sList = arrayOf("CupCake", "Donut", "Eclair", "Froyo", "GingerBread", "HoneyComb", "IceCream Sandwich",
-        //  "Jelly Bean", "KitKat", "Lollipop", "Marshmallow", "Nougat")
+          //  println(manga.name)
+           // println(manga.id)
+          //  println(manga.toString())
+      //  }
 
-
-        internal var sList = arrayOf( mangas.name        )
-        
-        //data class Manga (val name: String = "", val id: Int = 0)
-
-       // fun sList(args: Array<String>) {        Manga1   ,  Manga2 }
-
-
-      //var Manga1 = Manga (1,"trouducul")
-       // var Manga2 = Manga (2,"chapeaupointu")
+            var sList = arrayListOf<String>(println(manga.name))
 
 
 
-        //internal var sList = arrayOf(Manga1,Manga2)
 
+            private val mInflator: LayoutInflater
 
-        private val mInflator: LayoutInflater
-
-        init {
-            this.mInflator = LayoutInflater.from(context)
-        }
-
-        override fun getCount(): Int {
-            return sList.size
-        }
-
-        override fun getItem(position: Int): Any {
-            return sList[position]
-
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-            val view: View?
-            val vh: ListRowHolder
-            if (convertView == null) {
-                view = this.mInflator.inflate(R.layout.manga, parent, false)
-                vh = ListRowHolder(view)
-                view.tag = vh
-            } else {
-                view = convertView
-                vh = view.tag as ListRowHolder
+            init {
+                this.mInflator = LayoutInflater.from(context)
             }
 
+            override fun getCount(): Int {
+                return sList.size
+            }
 
-            vh.label.text = sList[position]
+            override fun getItem(position: Int): Any {
+                return sList[position]
+
+            }
+
+            override fun getItemId(position: Int): Long {
+                return position.toLong()
+            }
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+                val view: View?
+                val vh: ListRowHolder
+                if (convertView == null) {
+                    view = this.mInflator.inflate(R.layout.manga, parent, false)
+                    vh = ListRowHolder(view)
+                    view.tag = vh
+                } else {
+                    view = convertView
+                    vh = view.tag as ListRowHolder
+                }
 
 
-           // this.Manga1 = view?.findViewById<TextView>(R.id.manganame) as Manga
+                vh.label.text = sList[position]
 
-            return view
+
+
+
+                return view
+            }
+        }
+
+        private class ListRowHolder(row: View?) {
+            public val label: TextView
+
+            init {
+                this.label = row?.findViewById(R.id.MangaLabelName) as TextView
+            }
         }
     }
-
-    private class ListRowHolder(row: View?) {
-        public val label: TextView
-
-        init {
-            this.label = row?.findViewById(R.id.manganame) as TextView
-        }
-    }
-}
 
 
 
--- app/src/main/java/fr/parlonsmangafrancais/www/pmf/Manga.kt
+++ app/src/main/java/fr/parlonsmangafrancais/www/pmf/Manga.kt
@@ -3,6 +3,43 @@ package fr.parlonsmangafrancais.www.pmf
 import android.media.Image
 import java.util.*
 
-data class Manga constructor (val id: Long,var name : String ) {
+data class Manga  (val name : String = "", val id: Int =0 ) {
 
 }
+
+  //  data class VideoGame(val name: String, val publisher: String, var reviewScore: Int)
+   // val game: VideoGame = VideoGame("Gears of War", "Epic Games", 8)
+
+//game.component1() // name
+//game.component2() // publisher
+//game.component3() // reviewScore
+
+//fun copy(name: String = this.name , id: Int = this.id ) = Manga(name, id)
+
+
+
+fun main(args: Array<String>) {
+
+    val manga= Manga( "Epic Games",1)
+
+    val manga2 = Manga("hehe",2)
+
+
+
+    fun getMangaInfo() = {
+
+        val manga = Manga("Epic Games", 1)
+        val (name, id) = manga
+
+        println(manga.name)
+        println(manga.id)
+        println(manga.toString())
+
+      //  var sList = arrayListOf<String>(manga.name)
+    }
+
+
+
+
+}
+

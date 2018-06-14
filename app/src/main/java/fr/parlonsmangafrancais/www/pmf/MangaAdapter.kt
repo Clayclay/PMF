6e3e17e86ff051a7065a0e0951edd2431933d458
Marie <Mariepolmontiel@gmail.com>
2018-06-14 19:38:32 +0900

    travail sur Tome en cours
    Affichage des mangas Reussis

--- app/src/main/java/fr/parlonsmangafrancais/www/pmf/MangaAdapter.kt
+++ app/src/main/java/fr/parlonsmangafrancais/www/pmf/MangaAdapter.kt
@@ -1,9 +1,6 @@
 package fr.parlonsmangafrancais.www.pmf
 
-import android.annotation.SuppressLint
-import android.graphics.Color
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -12,14 +9,9 @@ import android.widget.ImageView
 
 import com.bumptech.glide.Glide
 
-import kotlinx.android.synthetic.main.activity_main.view.*
+
 import kotlinx.android.synthetic.main.manga_layout.view.*
-import com.bumptech.glide.load.resource.bitmap.TransformationUtils.centerCrop
-import com.bumptech.glide.request.RequestOptions
 import java.net.URL
-import android.graphics.Bitmap
-
-
 
 
 // L ADAPTATEUR POUR TRANSMETTRE DU JSON A MANGA.KT
@@ -49,10 +41,9 @@ class MangaAdapter(
 
 
 
+
+
     // Quoi afficher / The ViewHolder of the adapter
-
-
-
 
     class MangaHolder(mangaView: View): RecyclerView.ViewHolder(mangaView) {
 
@@ -85,22 +76,17 @@ class MangaAdapter(
                 println( tome.name)
         }*/
 
-////////////////* Afficher L'image du Manga */////////////////////
 
-println(manga.metaAPI["wpcf-image-principale"])
+////////////////* Afficher L'image du Manga *////////////////////////////
 
               val imageView = findViewById(R.id.imageMangaView) as ImageView
+              val imgURL =  manga.metaAPI["wpcf-image-principale"]?.component1().toString()
 
-              val imgURL =  manga.metaAPI["wpcf-image-principale"].toString()
-
-              // A besoin de " imgURL"
-
-              Glide.with(context)
-                      .load(imgURL)
-
+                Glide.with(this)
+                      .load(imgURL )
                       .into(imageView)
 
-
+////////////////* FIN -- Afficher L'image du Manga */////////////////////
 
             setOnClickListener { listener(manga) }
 
